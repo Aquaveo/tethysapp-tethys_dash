42a510d85b597eb4cd1c32542addaad835f7efa3
romer8 <gio.rombus@gmail.com>
2025-09-30 11:09:55 -0600

    update gitignore

--- reactapp/components/inputs/Custom.js
+++
@@ -1,4 +0,0 @@
-export { AddMapLayer } from "components/inputs/custom/AddMapLayer";
-export { MapExtent } from "components/inputs/custom/MapExtent";
-export { MapDrawing } from "components/inputs/custom/MapDrawing";
-export { default as SliderMetadata } from "components/inputs/custom/SliderMetadata";
--- tethysapp/tethysdash/exceptions.py
+++
@@ -1,4 +0,0 @@
-class VisualizationError(Exception):
-    """Raised for custom visualization messaging errors."""
-
-    pass
